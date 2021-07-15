695c91c6147ce18596002d18fa79fa369584bd54
Andreia Lima <andreialima7319@gmail.com>
2021-07-15 13:29:29 -0400

    edited the text field

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,7 +48,8 @@ var saveTasks = function() {
 
 
 
-// modal was triggered
+// modal was triggered//The following code block in script.js captures that button click:// in regular JS this would be document.querySelector("#task-form-modal .btn-primary").addEventListener("click", function() { });
+
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
   $("#modalTaskDescription, #modalDueDate").val("");
@@ -82,6 +83,90 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+// task text was clicked
+$(".list-group").on("click", "p", function() {
+  // get current text of p element
+  var text = $(this)
+    .text()
+    .trim();
+
+  // replace p element with a new textarea
+  var textInput = $("<textarea>").addClass("form-control").val(text);
+  $(this).replaceWith(textInput);
+
+  // auto focus new element
+  textInput.trigger("focus");
+});
+
+// editable field was un-focused
+$(".list-group").on("blur", "textarea", function() {
+  // get current value of textarea
+  var text = $(this).val();
+
+  // get status type and position in the list
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].text = text;
+  saveTasks();
+
+  // recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  // replace textarea with new content
+  $(this).replaceWith(taskP);
+});
+
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+  $(this).replaceWith(dateInput);
+
+  // automatically bring up the calendar
+  dateInput.trigger("focus");
+});
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  var date = $(this).val();
+
+  // get status type and position in the list
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span and insert in place of input element
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+    $(this).replaceWith(taskSpan);
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -95,3 +180,22 @@ $("#remove-tasks").on("click", function() {
 loadTasks();
 
 
+
+
+
+//Note the use of jQuery methods like addClass() and text() and Bootstrap classes like list-group-item and badge. The addClass() method updates an element's class attribute with new values while the text() method changes the element's text content (similar to element.textContent = ""; in plain JavaScript).//
+
+//The saveTasks() function simply saves the tasks object in localStorage, as we can see here:
+
+//var saveTasks = function() {
+ // localStorage.setItem("tasks", JSON.stringify(tasks));
+//};
+
+//Event delegation refers to offsetting the click event to a parent that will always exist, then checking which child element triggered the event. A plain JavaScript solution would look something like this:
+
+// document.querySelector("#wrapper").addEventListener("click", function(event) {
+//   if (event.target.matches(".task")) {
+//     console.log("dynamic task was clicked");
+//   }
+//  }); 
+//There's a shorthand jQuery method for event delegation using the on() method.
